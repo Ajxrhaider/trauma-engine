f7a9d4248007e9dc1b43541e327046a6fff4bdc9
Ajxrhaider <99840452+Ajxrhaider@users.noreply.github.com>
2026-01-01 14:24:30 +0100

    a1.5

---
+++ c++ tutorials/tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <limits>
+
+int main ()
+{
+    std::cout << "Hello, World!";
+    std::cout << "Hello, World!";
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\\n');
+    std::cin.get();
+    return 0;
+}
